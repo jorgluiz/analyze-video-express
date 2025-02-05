d5da893b34e57dd779c6240f6705e193996ce001
jorgluiz <jorgeluizjr20@gmail.com>
2025-02-04 22:50:40 -0300

    install ytdl-core

--- src/middlewares.js
+++ src/middlewares.js
@@ -31,16 +31,42 @@
 //   return 0;
 // }
 
+// _______________________________________________________________________________________________
 
-import ytdl from '@distube/ytdl-core';
+// import ytdl from '@distube/ytdl-core';
+
+// export async function getVideoDuration(urlVideo) {
+//   try {
+//     // Obtém os dados do vídeo
+//     const info = await ytdl.getInfo(urlVideo);
+
+//     // A duração do vídeo está disponível dentro de 'formats'
+//     const durationInSeconds = info.videoDetails.lengthSeconds;
+
+//     if (durationInSeconds <= 660) {
+//       console.log('Vídeo aceito. Duração:', durationInSeconds, 'segundos');
+//       return durationInSeconds;
+//     } else {
+//       console.log('Vídeo não aceito. Duração:', durationInSeconds, 'segundos');
+//       return null; // Ou pode lançar um erro
+//     }
+//   } catch (error) {
+//     console.error('Erro ao obter duração do vídeo:', error);
+//   }
+// }
+
+//_______________________________________________________________________________________________
+
+
+import ytdl from 'ytdl-core';
 
 export async function getVideoDuration(urlVideo) {
   try {
-    // Obtém os dados do vídeo
+    // Obtém os metadados do vídeo
     const info = await ytdl.getInfo(urlVideo);
 
-    // A duração do vídeo está disponível dentro de 'formats'
-    const durationInSeconds = info.videoDetails.lengthSeconds;
+    // A duração do vídeo em segundos
+    const durationInSeconds = parseInt(info.videoDetails.lengthSeconds, 10);
 
     if (durationInSeconds <= 660) {
       console.log('Vídeo aceito. Duração:', durationInSeconds, 'segundos');
@@ -51,5 +77,6 @@ export async function getVideoDuration(urlVideo) {
     }
   } catch (error) {
     console.error('Erro ao obter duração do vídeo:', error);
+    return null;
   }
 }
