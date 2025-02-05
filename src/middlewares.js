e82a1851608f46df25f7435c386133f8f50e1575
jorgluiz <jorgeluizjr20@gmail.com>
2025-02-05 07:05:08 -0300

    feat: replace ytdl-core with yt-scraper to get video duration

--- src/middlewares.js
+++ src/middlewares.js
@@ -58,22 +58,54 @@
 //_______________________________________________________________________________________________
 
 
-import ytdl from 'ytdl-core';
+// import ytdl from 'ytdl-core';
+
+// export async function getVideoDuration(urlVideo) {
+//   try {
+//     // Obtém os metadados do vídeo
+//     const info = await ytdl.getInfo(urlVideo);
+
+//     // A duração do vídeo em segundos
+//     const durationInSeconds = parseInt(info.videoDetails.lengthSeconds, 10);
+
+//     if (durationInSeconds <= 660) {
+//       console.log('Vídeo aceito. Duração:', durationInSeconds, 'segundos');
+//       return durationInSeconds;
+//     } else {
+//       console.log('Vídeo não aceito. Duração:', durationInSeconds, 'segundos');
+//       return null; // Ou pode lançar um erro
+//     }
+//   } catch (error) {
+//     console.error('Erro ao obter duração do vídeo:', error);
+//     return null;
+//   }
+// }
+
+//_______________________________________________________________________________________________
+
+
+import ytScraper from 'yt-scraper';
 
 export async function getVideoDuration(urlVideo) {
   try {
     // Obtém os metadados do vídeo
-    const info = await ytdl.getInfo(urlVideo);
+    const videoData = await ytScraper.videoInfo(urlVideo);
 
-    // A duração do vídeo em segundos
-    const durationInSeconds = parseInt(info.videoDetails.lengthSeconds, 10);
+    console.log("Duração do vídeo:", videoData.length);
+
+    if (!videoData || videoData.length == null) {
+      throw new Error('Não foi possível obter a duração do vídeo.');
+    }
+
+    // Como `videoData.length` já está em segundos, não precisa de conversão
+    const durationInSeconds = Number(videoData.length);
 
     if (durationInSeconds <= 660) {
       console.log('Vídeo aceito. Duração:', durationInSeconds, 'segundos');
       return durationInSeconds;
     } else {
       console.log('Vídeo não aceito. Duração:', durationInSeconds, 'segundos');
-      return null; // Ou pode lançar um erro
+      return null;
     }
   } catch (error) {
     console.error('Erro ao obter duração do vídeo:', error);
