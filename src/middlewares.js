230a9211386582cf86385a90f82b6d8456ba158b
jorgluiz <jorgeluizjr20@gmail.com>
2025-02-04 22:42:49 -0300

    Updates the getVideoDuration function to use the @distube/ytdl-core library.

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,32 +1,55 @@
-import youtubedl from 'youtube-dl-exec';
+// import youtubedl from 'youtube-dl-exec';
+
+// export async function getVideoDuration(urlVideo) {
+//   try {
+//     const duration = await youtubedl(urlVideo, { getDuration: true });
+
+//     // Converte a duração para segundos
+//     const durationInSeconds = convertDurationToSeconds(duration);
+//     // console.log(durationInSeconds)
+
+//     if (durationInSeconds <= 660) {
+//       console.log('Vídeo aceito. Duração:', duration);
+//       return duration;
+//     } else {
+//       return null; // Ou pode lançar um erro
+//     }
+//   } catch (error) {
+//     console.error('Erro ao obter duração do vídeo:', error);
+//   }
+// }
+
+// // Função para converter "HH:MM:SS" ou "MM:SS" em segundos
+// function convertDurationToSeconds(duration) {
+//   const parts = duration.split(':').map(Number);
+
+//   if (parts.length === 3) {
+//     return parts[0] * 3600 + parts[1] * 60 + parts[2]; // HH:MM:SS
+//   } else if (parts.length === 2) {
+//     return parts[0] * 60 + parts[1]; // MM:SS
+//   }
+//   return 0;
+// }
+
+
+import ytdl from '@distube/ytdl-core';
 
 export async function getVideoDuration(urlVideo) {
   try {
-    const duration = await youtubedl(urlVideo, { getDuration: true });
+    // Obtém os dados do vídeo
+    const info = await ytdl.getInfo(urlVideo);
 
-    // Converte a duração para segundos
-    const durationInSeconds = convertDurationToSeconds(duration);
-    // console.log(durationInSeconds)
+    // A duração do vídeo está disponível dentro de 'formats'
+    const durationInSeconds = info.videoDetails.lengthSeconds;
 
     if (durationInSeconds <= 660) {
-      console.log('Vídeo aceito. Duração:', duration);
-      return duration;
+      console.log('Vídeo aceito. Duração:', durationInSeconds, 'segundos');
+      return durationInSeconds;
     } else {
+      console.log('Vídeo não aceito. Duração:', durationInSeconds, 'segundos');
       return null; // Ou pode lançar um erro
     }
   } catch (error) {
     console.error('Erro ao obter duração do vídeo:', error);
   }
-}
-
-// Função para converter "HH:MM:SS" ou "MM:SS" em segundos
-function convertDurationToSeconds(duration) {
-  const parts = duration.split(':').map(Number);
-
-  if (parts.length === 3) {
-    return parts[0] * 3600 + parts[1] * 60 + parts[2]; // HH:MM:SS
-  } else if (parts.length === 2) {
-    return parts[0] * 60 + parts[1]; // MM:SS
-  }
-  return 0;
 }
